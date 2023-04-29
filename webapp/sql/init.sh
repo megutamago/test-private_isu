c5346f25afaa85f84fc55f95a5667046a8954a66
megutamago <naoya05280708@gmail.com>
2023-04-29 23:08:10 +0900

    submit

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -xu -o pipefail
+set -ex
+cd `dirname $0`
 
-CURRENT_DIR=$(cd $(dirname $0);pwd)
 export MYSQL_HOST=${MYSQL_HOST:-127.0.0.1}
 export MYSQL_PORT=${MYSQL_PORT:-3306}
 export MYSQL_USER=${MYSQL_USER:-isuconp}
 export MYSQL_DBNAME=${MYSQL_DBNAME:-isuconp}
 export MYSQL_PWD=${MYSQL_PASS:-isuconp}
 export LANG="C.UTF-8"
-cd $CURRENT_DIR
 
 cat 0_Schema.sql | mysql --defaults-file=/etc/mysql/my.cnf -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
