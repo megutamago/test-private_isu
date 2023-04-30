e7b7621086be8a84485e2abd1b472522c8a29acf
megutamago <naoya05280708@gmail.com>
2023-04-30 22:08:48 +0900

    submit

--- webapp/golang/app.go
+++ webapp/golang/app.go
@@ -858,4 +858,33 @@ func main() {
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", r))
+
+	# redis
+	redisClient := newClient()
+	result, err := ping(redisClient)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
+
+
+# redis
+func newRedisClient() *redis.Client {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "", // no password
+		DB: 0, // default DB
+	})
+	return redisClient
+}
+
+func redisPing(client *redis.Client) (string, error) {
+	result, err := client.Ping().Result()
+	if err != nil {
+		return "", err
+	} else {
+		return result, nil
+	}
 }
